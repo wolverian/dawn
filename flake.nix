ac64162831ad801422d0b4a9779ec1dd7c82bdc8
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2021-11-30 16:39:02 +0200

    nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        packages = flake-utils.lib.flattenTree {
+          gnumake = pkgs.gnumake;
+          gitAndTools = pkgs.gitAndTools;
+        };
+        defaultPackage = packages.gnumake;
+        apps.gnumake = flake-utils.lib.mkApp
+          {
+            drv = packages.gnumake;
+            name = "make";
+          };
+        defaultApp = apps.gnumake;
+      }
+    );
+}
